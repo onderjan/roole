5d2519ece7bac4dbd762cdb66ad88d3f7d4cbb53
Jan Onderka <onderjan@onderjan.net>
2026-01-21 14:45:01 +0100

    Fix bitwise not in linear being wrong

--- src/domain/bitvector/abstr/linear/bitwise.rs
+++ src/domain/bitvector/abstr/linear/bitwise.rs
@@ -2,24 +2,27 @@ use crate::domain::{
     bitvector::{
         BitvectorBound,
         abstr::{BitvectorDomain, linear::LinearBitvector},
+        concr::ConcreteBitvector,
     },
-    traits::forward::Bitwise,
+    traits::forward::{Bitwise, HwArith},
 };
 
 impl<B: BitvectorBound> Bitwise for LinearBitvector<B> {
-    fn bit_not(mut self) -> Self {
-        let Some(combination) = &mut self.combination else {
+    fn bit_not(self) -> Self {
+        // bit_not(x) = arith_neg(x) - 1
+
+        let mut arith_neg = self.arith_neg();
+
+        let Some(combination) = &mut arith_neg.combination else {
             // already top value
-            return self;
+            return arith_neg;
         };
 
-        combination.constant = combination.constant.bit_not();
+        combination.constant = combination
+            .constant
+            .sub(ConcreteBitvector::one(arith_neg.bound));
 
-        for coeff in combination.coefficients.values_mut() {
-            *coeff = (*coeff).bit_not();
-        }
-
-        self
+        arith_neg
     }
     fn bit_and(self, rhs: Self) -> Self {
         assert_eq!(self.bound, rhs.bound);
