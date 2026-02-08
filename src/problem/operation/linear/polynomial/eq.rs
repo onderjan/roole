815300346a72800c0197fa907a9af8f70bb06dee
Jan Onderka <onderjan@onderjan.net>
2026-02-08 17:05:01 +0100

    Extend resolving ITE with polynomials by assumptions

--- src/problem/operation/linear/polynomial/eq.rs
+++ src/problem/operation/linear/polynomial/eq.rs
@@ -1,4 +1,7 @@
-use crate::{domain::traits::forward::BExt, problem::operation::LinearPolynomial};
+use crate::{
+    domain::{bitvector::BitvectorBound, traits::forward::BExt},
+    problem::operation::LinearPolynomial,
+};
 
 impl LinearPolynomial {
     pub fn ite(
@@ -8,11 +11,12 @@ impl LinearPolynomial {
     ) -> Result<LinearPolynomial, ()> {
         let bound = then_branch.bound();
         assert_eq!(bound, else_branch.bound());
+        assert_eq!(condition.bound().width(), 1);
 
-        // we can represent ite as condition * (then - else) + else
+        // we can represent ite as else + condition * (then - else)
         // set truth = then - else
 
-        let mut truth = then_branch.sub(else_branch.clone());
+        let mut truth = then_branch.clone().sub(else_branch.clone());
 
         // if condition is constant, we can simplify ite to a polynomial
         if let Some(condition) = condition.constant_value() {
@@ -22,15 +26,28 @@ impl LinearPolynomial {
             return Ok(truth.add(else_branch));
         }
 
-        // if truth is constant and we can unsigned-extend condition to truth size,
+        // if we can unsigned-extend condition to truth size and we can get truth constant value with it assumed
         // we can simplify ite to a polynomial
-        if let Some(truth) = truth.constant_value()
-            && let Ok(mut extended_conditional) = condition.uext(bound)
+        if let Ok(mut extended_condition) = condition.clone().uext(bound)
+            && let Some(truth) = truth.constant_value_with_assumption(&condition)
         {
             // truth is constant, scale condition by it and add else branch
-            extended_conditional.scale(truth);
+            extended_condition.scale(truth);
+            return Ok(else_branch.add(extended_condition));
+        }
 
-            return Ok(extended_conditional.add(else_branch));
+        // try the same thing with negated condition
+        // note that the negated condition must be assumed instead of normal
+
+        let not_condition = condition.clone().bit_not();
+        if let Ok(mut extended_not_condition) = not_condition.clone().uext(bound)
+            && let Some(truth) = truth.constant_value_with_assumption(&not_condition)
+        {
+            // we can represent as then + not_condition * (else - then)
+            // and so as then - not_condition * (then - else)
+            extended_not_condition.scale(truth);
+
+            return Ok(then_branch.sub(extended_not_condition));
         }
 
         Err(())
