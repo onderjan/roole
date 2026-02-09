72e6f2fdce3de99604cff1642918ff9bff4f328f
Jan Onderka <onderjan@onderjan.net>
2026-02-09 12:32:52 +0100

    Fix evaluating polynomials
    The slice width was not properly taken into account: after logical shift right to take care of lsb, unsigned extension to slice width must be done, followed by unsigned extension to polynomial result width.

--- src/problem/operation/linear/polynomial.rs
+++ src/problem/operation/linear/polynomial.rs
@@ -93,7 +93,6 @@ impl LinearPolynomial {
     pub fn evaluate<D: EvaluableDomain>(&self, fetch: impl Fn(FormulaId) -> D) -> D {
         let mut value = D::single_value(self.constant_term);
         let polynomial_bound = value.bound();
-        let polynomial_width = polynomial_bound.width();
 
         for (slice, coefficient) in &self.linear_terms {
             let mut formula_value = (fetch)(slice.formula_id);
@@ -105,9 +104,16 @@ impl LinearPolynomial {
                 formula_value = formula_value.logic_shr(D::single_value(lsb));
             }
 
-            // unless slice lsb is equal to zero and formula value width is equal to polynomial width,
-            // perform unsigned extension
-            if slice.lsb != 0 || formula_value.bound().width() != polynomial_width {
+            let slice_bound = RBound::new(slice.width.get());
+
+            // perform unsigned extension to slice width unless it is a no-op
+            if formula_value.bound() != slice_bound {
+                formula_value = formula_value.uext(slice_bound);
+            }
+
+            // unless the slice width is equal to polynomial width,
+            // perform unsigned extension to it
+            if slice_bound != polynomial_bound {
                 formula_value = formula_value.uext(polynomial_bound);
             }
 
