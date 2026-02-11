6c9dc1af8d4adc855bbcdef79e5a27aaa16ccc05
Jan Onderka <onderjan@onderjan.net>
2026-02-11 12:09:29 +0100

    Make monomial debug print similar to polynomial

--- src/problem/symbolic/linear/monomial.rs
+++ src/problem/symbolic/linear/monomial.rs
@@ -1,9 +1,11 @@
+use std::fmt::{Debug, UpperHex};
+
 use serde::{Deserialize, Serialize};
 
 use super::LinearSlice;
 use crate::domain::bitvector::{BitvectorBound, RBound, concr::ConcreteBitvector};
 
-#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct LinearMonomial {
     pub coefficient: ConcreteBitvector<RBound>,
     pub slice: LinearSlice,
@@ -33,5 +35,34 @@ impl LinearMonomial {
         let max_value_allowed = self.coefficient.bound().allowed(max_value);
 
         !max_value_allowed
+    }
+
+    pub(super) fn format(&self, f: &mut std::fmt::Formatter<'_>, hex: bool) -> std::fmt::Result {
+        if !self.coefficient.is_one() {
+            if hex {
+                write!(f, "{:#X}*", self.coefficient)?;
+            } else {
+                write!(f, "{:?}*", self.coefficient)?;
+            }
+        }
+
+        write!(f, "{:?} mod ", self.slice)?;
+        if hex {
+            write!(f, "{:#X}", 1u128 << self.bound().width())
+        } else {
+            write!(f, "{:?}", 1u128 << self.bound().width())
+        }
+    }
+}
+
+impl Debug for LinearMonomial {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.format(f, false)
+    }
+}
+
+impl UpperHex for LinearMonomial {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.format(f, true)
     }
 }
