c15e06c04c389cbc81696d7d3bedc2f33baf86d6
Jan Onderka <onderjan@onderjan.net>
2026-04-14 19:10:01 +0200

    Reimplement sdiv/srem by quadrants

--- src/domain/bitvector/concr/arith.rs
+++ src/domain/bitvector/concr/arith.rs
@@ -56,48 +56,64 @@ impl<B: BitvectorBound> HwArith for ConcreteBitvector<B> {
         Self::from_masked(value, self.bound)
     }
 
-    fn sdiv_wrapping_by_quadrants(self, rhs: Self) -> Self {
-        todo!("Sdiv");
-        /*
-        let bound = self.bound;
-        assert_eq!(bound, rhs.bound);
+    fn sdiv_wrapping_by_quadrants(mut self, mut rhs: Self) -> Self {
+        let negative_lhs = self.is_sign_bit_set();
+        let negative_rhs = rhs.is_sign_bit_set();
 
-        if rhs.is_zero() {
-            // the value to return depends on the sign of dividend
-            return if self.is_sign_bit_set() {
-                // return one
-                ConcreteBitvector::new_one(bound)
-            } else {
-                // return all-ones
-                ConcreteBitvector::new_all_ones(bound)
-            };
+        if negative_lhs {
+            self = self.arith_neg();
+        }
+        if negative_rhs {
+            rhs = rhs.arith_neg();
         }
 
-        let dividend = self.to_i64();
-        let divisor = rhs.to_i64();
+        let mut result = self.udiv_wrapping_or_all_ones(rhs);
 
-        let result = dividend.wrapping_div(divisor);
-        Self::from_masked_u64(result as u64, bound)
-        */
+        // negate the result exactly if just one is negative
+        if negative_lhs ^ negative_rhs {
+            result = result.arith_neg();
+        }
+
+        result
     }
 
-    fn srem_wrapping_by_quadrants(self, rhs: Self) -> Self {
-        todo!("Srem");
-        /*
-        let bound = self.bound;
-        assert_eq!(bound, rhs.bound);
+    fn srem_wrapping_by_quadrants(mut self, mut rhs: Self) -> Self {
+        let negative_lhs = self.is_sign_bit_set();
+        let negative_rhs = rhs.is_sign_bit_set();
 
-        if rhs.is_zero() {
-            // return dividend
-            return self;
+        if negative_lhs {
+            self = self.arith_neg();
+        }
+        if negative_rhs {
+            rhs = rhs.arith_neg();
         }
 
-        let dividend = self.to_i64();
-        let divisor = rhs.to_i64();
+        let mut result = self.urem_wrapping_or_dividend(rhs);
 
-        let result = dividend.wrapping_rem(divisor);
-        Self::from_masked_u64(result as u64, bound)
-        */
+        // negate the result exactly if the dividend is negative
+        if negative_lhs {
+            result = result.arith_neg();
+        }
+
+        result
+    }
+}
+
+fn udiv_wrapping_or_all_ones_u64(a: u64, b: u64) -> u64 {
+    if b != 0 {
+        a.wrapping_div(b)
+    } else {
+        // return all ones
+        u64::MAX
+    }
+}
+
+fn urem_wrapping_or_dividend_u64(a: u64, b: u64) -> u64 {
+    if b != 0 {
+        a.wrapping_rem(b)
+    } else {
+        // return dividend
+        a
     }
 }
 
